611a3f32bf5f79f948ecef696efd4cc5fd4e8fbc
freilinjb <freilinjb@gmail.com>
2020-04-17 15:47:18 -0400

    enviando datos al hooks actualizarState

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    const [busqueda, setBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    });
+
+    const actualizarState = e =>{
+        setBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const { artista, cancion } = busqueda;
+
     return ( 
-        <div className="bg-info">
+        <div className="bg-info"> 
             <div className="container">
                 <div className="row">
                     <form className="col text-white bg-transparent mb-5 pt-5 pb-2">
@@ -13,13 +28,13 @@ const Formulario = () => {
                             <div className="col-md-6 col-sm-12">
                                 <div className="form-group">
                                 <label htmlFor="artista">Artista</label>
-                                <input type="text" name="artista" className="form-control" placeholder="Nombre del Artista" aria-describedby="helpId"/>
+                                <input type="text" name="artista" value={artista} className="form-control" placeholder="Nombre del Artista" onChange={actualizarState}/>
                                 </div>
                             </div>
                             <div className="col-md-6 col-sm-12">
                                 <div className="form-group">
                                     <label htmlFor="cancion">Cancion</label>
-                                    <input type="text" name="cancion" className="form-control" placeholder="Nombre del Artista" aria-describedby="helpId"/>
+                                    <input type="text" name="cancion" value={cancion} className="form-control" placeholder="Nombre del Artista" onChange={actualizarState}/>
                                 </div>
                             </div>
                         </div>
