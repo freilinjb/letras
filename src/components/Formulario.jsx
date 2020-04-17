7554580ba4fc3c879f4b3b6080820c06ffefb8b0
freilinjb <freilinjb@gmail.com>
2020-04-17 15:31:26 -0400

    creando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 
 const Formulario = () => {
     return ( 
-        <h1>Formulario</h1>
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form className="col text-white bg-transparent mb-5 pt-5 pb-2">
+                        <fieldset>
+                            <legend className="text-center">Buscador de Letras de Canciones</legend>
+                        </fieldset>
+                        <div className="row">
+                            <div className="col-md-6 col-sm-12">
+                                <div className="form-group">
+                                <label htmlFor="artista">Artista</label>
+                                <input type="text" name="artista" className="form-control" placeholder="Nombre del Artista" aria-describedby="helpId"/>
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-sm-12">
+                                <div className="form-group">
+                                    <label htmlFor="cancion">Cancion</label>
+                                    <input type="text" name="cancion" className="form-control" placeholder="Nombre del Artista" aria-describedby="helpId"/>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-danger float-right">Buscar</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
      );
 }
  
